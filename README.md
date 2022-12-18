eb06fd16e7478d147ef09e1a66dd4199736a8471
Mayra Pena(Peña) <97967943+mayrapena1324@users.noreply.github.com>
2022-12-18 16:46:46 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# morse-code-portfolio-project
+ This project takes strings as an input and returns the string in morse code.  
---
+++ main.py
@@ -0,0 +1,33 @@
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+
+def create_morse(message):
+    ciphered_text = ""
+    for letter in message:
+        if letter == " ":
+            ciphered_letter = " / "
+        else:
+            ciphered_letter = MORSE_CODE_DICT[letter]
+        ciphered_text += ciphered_letter + " "
+    return ciphered_text
+
+
+text = input("What would you like to translate to morse code? Type your text here: ").upper()
+encrypted_text = create_morse(text)
+print(encrypted_text)
+
