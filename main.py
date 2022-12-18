e40756b29dfacd18ce9cd4ceabf28ab6bd928f28
Mayra Pena(Peña) <97967943+mayrapena1324@users.noreply.github.com>
2022-12-18 17:25:04 -0600

    Add flask application to simple project Add Forms to simple project

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+#
+#  a) Get /morse route to work
+#  b) create morse code class
+#  c) template inheritance?
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SECRET_KEY'  #
+
+
 # Dictionary representing the morse code chart
 MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    'C': '-.-.', 'D': '-..', 'E': '.',
@@ -16,6 +31,11 @@ MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    '(': '-.--.', ')': '-.--.-'}
 
 
+class TextToEncrypt(FlaskForm):
+    text = StringField("Text", validators=[DataRequired()])
+    submit = SubmitField("Encrypt")
+
+
 def create_morse(message):
     ciphered_text = ""
     for letter in message:
@@ -27,7 +47,18 @@ def create_morse(message):
     return ciphered_text
 
 
-text = input("What would you like to translate to morse code? Type your text here: ").upper()
-encrypted_text = create_morse(text)
-print(encrypted_text)
+@app.route("/", methods=["GET", "POST"])
+def home():
+    form = TextToEncrypt()
+    return render_template("index.html", form=form)
 
+
+@app.route("/morse", methods=["GET", "POST"])
+def morse_text():
+    text = input("What would you like to translate to morse code? Type your text here: ").upper()
+    encrypted_text = create_morse(text)
+    return render_template("morse_text.html", message=encrypted_text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
