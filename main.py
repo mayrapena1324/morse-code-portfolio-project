57c95256f45785acf143b5bd5d68d2d799ee009e
Mayra Pena(Peña) <97967943+mayrapena1324@users.noreply.github.com>
2022-12-24 00:53:48 -0600

    add requirements.txt add morse API instead of hard coded dict

--- main.py
+++ main.py
@@ -4,35 +4,13 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from morse import Morse
 
-#  a) Find api that plays morse code text
-#  b) create morse code class
 #  c) add decrypt capabilities
 #  d) bootstrap
-#  e) host
-#  f) ensure all symbols are included in dict or find api that has letters
 #  h) add users and save their morse searches with postgreSQL database
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_KEY'
-
-
-# Dictionary representing the morse code chart
-MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
-                   'C': '-.-.', 'D': '-..', 'E': '.',
-                   'F': '..-.', 'G': '--.', 'H': '....',
-                   'I': '..', 'J': '.---', 'K': '-.-',
-                   'L': '.-..', 'M': '--', 'N': '-.',
-                   'O': '---', 'P': '.--.', 'Q': '--.-',
-                   'R': '.-.', 'S': '...', 'T': '-',
-                   'U': '..-', 'V': '...-', 'W': '.--',
-                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
-                   '1': '.----', '2': '..---', '3': '...--',
-                   '4': '....-', '5': '.....', '6': '-....',
-                   '7': '--...', '8': '---..', '9': '----.',
-                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
-                   '?': '..--..', '/': '-..-.', '-': '-....-',
-                   '(': '-.--.', ')': '-.--.-'}
 
 
 class TextToEncrypt(FlaskForm):
--- morse.py
+++ morse.py
@@ -1,28 +1,19 @@
-# Dictionary representing the morse code chart
-MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
-                   'C': '-.-.', 'D': '-..', 'E': '.',
-                   'F': '..-.', 'G': '--.', 'H': '....',
-                   'I': '..', 'J': '.---', 'K': '-.-',
-                   'L': '.-..', 'M': '--', 'N': '-.',
-                   'O': '---', 'P': '.--.', 'Q': '--.-',
-                   'R': '.-.', 'S': '...', 'T': '-',
-                   'U': '..-', 'V': '...-', 'W': '.--',
-                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
-                   '1': '.----', '2': '..---', '3': '...--',
-                   '4': '....-', '5': '.....', '6': '-....',
-                   '7': '--...', '8': '---..', '9': '----.',
-                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
-                   '?': '..--..', '!': '-.-.--', '/': '-..-.', '-': '-....-',
-                   '(': '-.--.', ')': '-.--.-'}
+import requests
 
 
 class Morse:
     def create_morse(self, message):
-        ciphered_text = ""
-        for letter in message:
-            if letter == " ":
-                ciphered_letter = " / "
-            else:
-                ciphered_letter = MORSE_CODE_DICT[letter]
-            ciphered_text += ciphered_letter + " "
+        parameters = {
+            "text": message,
+        }
+
+        response = requests.get("https://api.funtranslations.com/translate/morse/", params=parameters).json()
+        ciphered_text = response["contents"]["translated"]
+    #     ciphered_text = ""
+    #     for letter in message:
+    #         if letter == " ":
+    #             ciphered_letter = " / "
+    #         else:
+    #             ciphered_letter = MORSE_CODE_DICT[letter]
+    #         ciphered_text += ciphered_letter + " "
         return ciphered_text
