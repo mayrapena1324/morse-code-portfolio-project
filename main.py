abd96e4f47621b50a1fb3ead0b345a217ab6b631
Mayra Pena(Peña) <97967943+mayrapena1324@users.noreply.github.com>
2022-12-25 20:43:48 -0600

    fix styles sheet connection

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from morse import Morse
+import pyperclip
 
+# have morse copied to clip board
+# a nice css animation for a card that says Morse and underneath has two buttons. morsify, demorsify. buttons will route
+# to correct html form
 #  c) add decrypt capabilities
 #  d) bootstrap
 #  h) add users and save their morse searches with postgreSQL database
@@ -14,24 +18,36 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_KEY'
 Bootstrap(app)
 
+
 class TextToEncrypt(FlaskForm):
     text = StringField("Text", validators=[DataRequired()])
-    submit = SubmitField("Encrypt")
+    submit = SubmitField("Translate")
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    form = TextToEncrypt()
-    return render_template("index.html", form=form)
+    return render_template("index.html")
 
 
 @app.route("/morse", methods=["GET", "POST"])
 def morse_text():
     form = TextToEncrypt()
+    return render_template("encrypt.html", form=form)
+
+
+@app.route("/translate", methods=["GET", "POST"])
+def show_results():
+    form = TextToEncrypt()
     text = form.text.data
     morse = Morse()
-    message = morse.create_morse(text.upper())
+    message = morse.create_morse(text)
     return render_template("morse_text.html", message=message)
+
+
+@app.route("/english", methods=["GET", "POST"])
+def to_english():
+    form = TextToEncrypt()
+    return render_template("decrypt.html", form=form)
 
 
 if __name__ == '__main__':
--- morse.py
+++ morse.py
@@ -9,11 +9,19 @@ class Morse:
 
         response = requests.get("https://api.funtranslations.com/translate/morse/", params=parameters).json()
         ciphered_text = response["contents"]["translated"]
-    #     ciphered_text = ""
-    #     for letter in message:
-    #         if letter == " ":
-    #             ciphered_letter = " / "
-    #         else:
-    #             ciphered_letter = MORSE_CODE_DICT[letter]
-    #         ciphered_text += ciphered_letter + " "
+        #     ciphered_text = ""
+        #     for letter in message:
+        #         if letter == " ":
+        #             ciphered_letter = " / "
+        #         else:
+        #             ciphered_letter = MORSE_CODE_DICT[letter]
+        #         ciphered_text += ciphered_letter + " "
         return ciphered_text
+
+    def decrypt_morse(self, message):
+        parameters = {
+            "text": message,
+        }
+        response = requests.get("http://api.funtranslations.com/translate/morse2english", params=parameters).json()
+        english_text = response["contents"]["translated"]
+        return english_text
